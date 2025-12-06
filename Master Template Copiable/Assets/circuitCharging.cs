0bc1c603da45a942499d2bcae71add8246927113
QuinnWuest <qwuest123@gmail.com>
2025-12-05 21:55:51 -0500

    Done (i hope)

--- Master Template Copiable/Assets/circuitCharging.cs
+++ Master Template Copiable/Assets/circuitCharging.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -59,20 +59,6 @@ public class circuitCharging : MonoBehaviour
 
     private static readonly bool[][] brailleList = new string[26] { "100000", "110000", "100100", "100110", "100010", "110100", "110110", "110010", "010100", "010110", "101000", "111000", "101100", "101110", "101010", "111100", "111110", "111010", "011100", "011110", "101001", "111001", "010111", "101101", "101111", "101011" }.Select(i => i.Select(j => j == '1').ToArray()).ToArray(); //braille list stolen from angel hernandez
 
-    //the first digit after the c is the first component and the second is the second component
-    string[] c01characters;
-    char c02character;
-    int c02position;
-    string[] c10characters;
-    int[] c10positions;
-    int c10shift;
-    string c12segments;
-    string c12flips = "";
-    string c20character;
-    bool[] c20flips = new bool[6];
-    int[] c21positions = { -1, -1 };
-    string c21character;
-
     static readonly string[] wordBank = new string[]
        {
                 "ABACK", "ABIDE", "ABORT", "ABOUT", "ABOVE", "ABUSE", "ABYSS", "ACIDS", "ACORN", "ACRES", "ACTED", "ACTOR", "ACUTE", "ADAPT", "ADDED", "ADIEU", "ADIOS", "ADMIN", "ADOPT", "ADORE", "ADORN", "ADULT", "AFFIX", "AFTER", "AGAIN", "AGENT", "AGILE", "AGING", "AGONY", "AGORA", "AGREE", "AHEAD", "AIDED", "AIMED", "AIOLI", "AIRED", "AISLE", "ALARM", "ALBUM", "ALERT", "ALGAE", "ALIAS", "ALIBI", "ALIEN", "ALIGN", "ALIKE", "ALIVE", "ALLAY", "ALLEY", "ALLOT", "ALLOW", "ALLOY", "ALOFT", "ALONE", "ALONG", "ALOOF", "ALOUD", "ALPHA", "ALTAR", "ALTER", "AMASS", "AMAZE", "AMBER", "AMBLE", "AMEND", "AMISH", "AMISS", "AMONG", "AMPLE", "AMUSE", "ANGEL", "ANGER", "ANGLE", "ANGLO", "ANGRY", "ANGST", "ANIME", "ANION", "ANISE", "ANKLE", "ANNEX", "ANNOY", "ANNUL", "ANTIC", "ANVIL", "AORTA", "APART", "APNEA", "APPLE", "APPLY", "APRON", "AREAS", "ARENA", "ARGUE", "ARISE", "ARMED", "ARMOR", "AROMA", "AROSE", "ARRAY", "ARROW", "ARSON", "ASHEN", "ASHES", "ASIAN", "ASIDE", "ASKED", "ASSAY", "ASSET", "ASTER", "ASTIR", "ATOLL", "ATOMS", "ATONE", "ATTIC", "AUDIO", "AUDIT", "AUGUR", "AUNTY", "AVAIL", "AVIAN", "AVOID", "AWAIT", "AWAKE", "AWARD", "AWARE", "AWASH", "AWFUL", "AWOKE", "AXIAL", "AXIOM", "AXION", "AZTEC",
@@ -101,17 +87,18 @@ public class circuitCharging : MonoBehaviour
                 "YACHT", "YARDS", "YAWNS", "YEARN", "YEARS", "YEAST", "YELLS", "YIELD", "YODEL", "YOUNG", "YOURS", "YOUTH", "YUMMY",
                 "ZEBRA", "ZILCH", "ZONES"
        };
-    string chosenWord;
-    List<string> possibleWords;
-    List<string> possibleWordsThisGeneration = new List<string> { "adfahdsjgasldkfghasdgf" }; //this random keysmash is added to the list at the start so that c# doesn't shit its pants later down the line when i need to clear it to generate hints
-    int wordsLeft;
-    int wordsLeftThisGeneration = 9999;
 
-    int[][] forbiddenPairs = new int[2][];
-    int[][] generatedPairs = new int[4][];
-    int generationAttempts = 0;
-    int generationThreshold = wordBank.Length / 2;
-    int hintsGenerated = 0;
+    private static readonly string[] _hintTypes = new string[6]
+    {
+        "Light-Speaker",
+        "Light-Letter",
+        "Speaker-Light",
+        "Speaker-Letter",
+        "Letter-Light",
+        "Letter-Speaker"
+    };
+
+    string chosenWord;
 
     void Awake()
     {
@@ -139,52 +126,223 @@ public class circuitCharging : MonoBehaviour
         toggleLight(true);
     }
 
+    int li_sp_ix;
+    char[] li_sp_letters;
+
+    int li_le_ix;
+    char li_le_letter;
+    int li_le_flashes;
+
+    int[] sp_li_correctLetterIxs;
+    char[] sp_li_ogLetters;
+    char[] sp_li_adjustedLetters;
+
+    char sp_le_lastLetter;
+    bool[] sp_le_correctSegments;
+    bool[] sp_le_randomBeeps;
+    bool[] sp_le_adjustedSegments;
+
+    char le_li_firstLetter;
+    char le_li_randomLetter;
+    bool[] le_li_firstBraille;
+    bool[] le_li_randomBraille;
+    bool[] le_li_lightsToToggle;
+
+    int le_sp_letterIndex;
+    char le_sp_letter;
+    bool[] le_sp_segments;
+    int le_sp_otherPosition;
+    int le_sp_pos1;
+    int le_sp_pos2;
+
+    int[] bannedHints;
+
     void Start()
     {
-        chosenWord = wordBank[Rnd.Range(0, wordBank.Length)];
-        Debug.Log("<> Chosen word: " + chosenWord);
+        string finalSolution = null;
+        int attempts = 0;
 
-        // Light-Speaker Hint
-        var li_sp_ix = Rnd.Range(0, chosenWord.Length - 1);
-        var li_sp_letters = new[] { chosenWord[li_sp_ix], chosenWord[li_sp_ix + 1] }.Shuffle();
-        Debug.Log("<> Light-Speaker: " + li_sp_letters.Join(" "));
-
-        // Light-Letter Hint
-        var li_le = Rnd.Range(0, chosenWord.Length);
-        var li_le_letter = chosenWord[li_le];
-        var li_le_flashes = li_le + 1;
-        Debug.Log("<> Light-Letter: " + li_le_letter + ", " + li_le_flashes);
-
-        // Speaker-Light Hint
-        var sp_li_correctLetterIxs = Enumerable.Range(0, 5).ToArray().Shuffle().Take(2).OrderBy(i => i).ToArray();
-        var sp_li_letters = new char[5];
-        for (int i = 0; i < 5; i++)
+        while (attempts++ < 1000 && finalSolution == null)
         {
-            char sp_li_randomLetter;
-            if (!sp_li_correctLetterIxs.Contains(i))
-            {
-                var sp_li_possibleLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".Except(new[] { chosenWord[i] });
-                sp_li_randomLetter = sp_li_possibleLetters.PickRandom();
-                sp_li_letters[i] = sp_li_randomLetter;
-            }
-            else
-            {
-                sp_li_letters[i] = chosenWord[i];
-            }
-            if (sp_li_letters[i] < 'F')
-                sp_li_letters[i] = (char)(sp_li_letters[i] + 21);
-            else
-                sp_li_letters[i] = (char)(sp_li_letters[i] - 5);
-        }
-        Debug.Log("<> Speaker-Light: " + sp_li_letters.Join(", "));
+            chosenWord = wordBank[Rnd.Range(0, wordBank.Length)];
 
-        // Speaker-Letter Hint
-        var sp_le_lastLetter = chosenWord.Last() - 'A';
-        var sp_le_correctSegments = _segmentArragements[sp_le_lastLetter];
-        var sp_le_randomBeeps = Enumerable.Range(0, 14).Select(i => Rnd.Range(0, 2) == 0).ToArray();
-        var sp_le_adjustedSegments = Enumerable.Range(0, 14).Select(i => sp_le_correctSegments[i] ^ sp_le_randomBeeps[i]).ToArray();
-        Debug.LogFormat("<> Speaker-Letter: {0} {1}", sp_le_adjustedSegments.Select(i => i ? "1" : "0").Join(""), sp_le_randomBeeps.Select(i => i ? "1" : "0").Join(""));
-        return;
+            // Light–Speaker: two adjacent letters (order unknown)
+            li_sp_ix = Rnd.Range(0, chosenWord.Length - 1);
+            li_sp_letters = new[] { chosenWord[li_sp_ix], chosenWord[li_sp_ix + 1] }.Shuffle();
+
+            // Light–Letter: letter & its position
+            li_le_ix = Rnd.Range(0, chosenWord.Length);
+            li_le_letter = chosenWord[li_le_ix];
+            li_le_flashes = li_le_ix + 1;
+
+            // Speaker–Light: 5 letters, exactly 2 correct positions, ALL shifted backward 5
+            sp_li_correctLetterIxs = Enumerable.Range(0, 5).ToArray()
+                .Shuffle().Take(2).OrderBy(i => i).ToArray();
+
+            sp_li_ogLetters = new char[5];
+            sp_li_adjustedLetters = new char[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (sp_li_correctLetterIxs.Contains(i))
+                {
+                    sp_li_ogLetters[i] = chosenWord[i];
+                }
+                else
+                {
+                    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".Except(new[] { chosenWord[i] });
+                    sp_li_ogLetters[i] = possible.PickRandom();
+                }
+
+                char c = sp_li_ogLetters[i];
+                sp_li_adjustedLetters[i] = (c < 'F') ? (char)(c + 21) : (char)(c - 5);
+            }
+
+            // Speaker–Letter: last letter from segments + beeps
+            sp_le_lastLetter = chosenWord.Last();
+            sp_le_correctSegments = _segmentArragements[sp_le_lastLetter - 'A'];
+            sp_le_randomBeeps = Enumerable.Range(0, 14)
+                .Select(ix => Rnd.Range(0, 2) == 0).ToArray();
+            sp_le_adjustedSegments = sp_le_correctSegments
+                .Select((seg, idx) => seg ^ sp_le_randomBeeps[idx]).ToArray();
+
+            // Letter–Light: first letter via braille inversion
+            le_li_firstLetter = chosenWord.First();
+            le_li_randomLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                .Except(new[] { le_li_firstLetter }).PickRandom();
+
+            le_li_firstBraille = brailleList[le_li_firstLetter - 'A'];
+            le_li_randomBraille = brailleList[le_li_randomLetter - 'A'];
+
+            le_li_lightsToToggle = Enumerable.Range(0, 6)
+                .Select(ix => le_li_firstBraille[ix] ^ le_li_randomBraille[ix]).ToArray();
+
+            // Letter–Speaker: letter with one real and one faulty position
+            le_sp_letterIndex = Rnd.Range(0, 5);
+            le_sp_letter = chosenWord[le_sp_letterIndex];
+            le_sp_segments = _segmentArragements[le_sp_letter - 'A'].ToArray().Shuffle();
+
+            le_sp_otherPosition = Enumerable.Range(0, 5)
+                .Except(new[] { le_sp_letterIndex }).PickRandom();  // decoy position
+
+            le_sp_pos1 = le_sp_letterIndex;
+            le_sp_pos2 = le_sp_otherPosition;
+
+            // Filter all combos
+            var allCombos = wordBank.AsEnumerable();
+
+            bannedHints = Enumerable.Range(0, 6).ToArray()
+                .Shuffle().Take(2).OrderBy(x => x).ToArray();
+
+            for (int clue = 0; clue < 6; clue++)
+            {
+                if (bannedHints.Contains(clue))
+                    continue;
+
+                // Light–Speaker (two adjacent letters in either order)
+                if (clue == 0)
+                {
+                    char a = li_sp_letters[0];
+                    char b = li_sp_letters[1];
+
+                    allCombos = allCombos.Where(ix =>
+                        (ix[li_sp_ix] == a && ix[li_sp_ix + 1] == b) ||
+                        (ix[li_sp_ix] == b && ix[li_sp_ix + 1] == a));
+                }
+
+                // Light–Letter (letter at known position)
+                if (clue == 1)
+                {
+                    allCombos = allCombos.Where(ix => ix[li_le_ix] == li_le_letter);
+                }
+
+                // Speaker–Light (exactly 2 positions match the shifted-forward result)
+                if (clue == 2)
+                {
+                    var target = sp_li_ogLetters;
+
+                    allCombos = allCombos.Where(ix =>
+                        Enumerable.Range(0, 5).Count(p => ix[p] == target[p]) == 2);
+                }
+
+                // Speaker–Letter (last letter)
+                if (clue == 3)
+                {
+                    allCombos = allCombos.Where(ix => ix[4] == sp_le_lastLetter);
+                }
+
+                // Letter–Light (first letter)
+                if (clue == 4)
+                {
+                    allCombos = allCombos.Where(ix => ix[0] == le_li_firstLetter);
+                }
+
+                // Letter–Speaker
+                if (clue == 5)
+                {
+                    allCombos = allCombos.Where(ix => ix[le_sp_letterIndex] == le_sp_letter);
+                }
+            }
+
+            // Uniqueness check
+            var remaining = allCombos.Take(2).ToList();
+
+            if (remaining.Count == 1)
+                finalSolution = remaining[0];
+        }
+
+        if (finalSolution == null)
+        {
+            Debug.LogFormat("[Circuit Charging #{0}] Puzzle failed to generate after 1000 attempts!", ModuleId);
+            Module.HandlePass();
+            return;
+        }
+
+        Debug.LogFormat("[Circuit Charging #{0}] Chosen word: {1}", ModuleId, chosenWord);
+        Debug.LogFormat("[Circuit Charging #{0}] Banned hints: {1}", ModuleId, bannedHints.Select(i => _hintTypes[i]).Join(", "));
+
+        if (!bannedHints.Contains(0))
+            Debug.LogFormat("[Circuit Charging #{0}] Light-Speaker: Positions = {1} & {2}, Letters = {3}", ModuleId,
+                li_sp_ix + 1, li_sp_ix + 2, li_sp_letters.Join(" "));
+
+        if (!bannedHints.Contains(1))
+            Debug.LogFormat("[Circuit Charging #{0}] Light-Letter: Letter = {1}, Position = {2}", ModuleId,
+                li_le_letter, li_le_flashes);
+
+        if (!bannedHints.Contains(2))
+            Debug.LogFormat("[Circuit Charging #{0}] Speaker-Light: OG letters = {1}, Adjusted letters = {2}, Correct positions = {3}", ModuleId,
+                sp_li_ogLetters.Join(""),
+                sp_li_adjustedLetters.Join(""),
+                sp_li_correctLetterIxs.Join(" "));
+
+        if (!bannedHints.Contains(3))
+            Debug.LogFormat("[Circuit Charging #{0}] Speaker-Letter: Segments = {1}, Beeps = {2} (Last letter = {3})", ModuleId,
+                sp_le_adjustedSegments.Select(i => i ? "1" : "0").Join(""),
+                sp_le_randomBeeps.Select(i => i ? "1" : "0").Join(""),
+                sp_le_lastLetter);
+
+        if (!bannedHints.Contains(4))
+            Debug.LogFormat("[Circuit Charging #{0}] Letter-Light: Random letter = {1}, Flashes = {2} (First letter = {3})", ModuleId,
+                le_li_randomLetter,
+                le_li_lightsToToggle.Select(i => i ? "1" : "0").Join(""),
+                le_li_firstLetter);
+
+        if (!bannedHints.Contains(5))
+            Debug.LogFormat("[Circuit Charging #{0}] Letter-Speaker: Random position = {1}, Faulty position = {2}, Letter = {3}", ModuleId,
+                le_sp_letterIndex + 1,
+                le_sp_otherPosition + 1,
+                le_sp_letter);
+    }
+
+
+    public static IEnumerable<string> GenerateAllFiveLetterCombos()
+    {
+        for (char a = 'A'; a <= 'Z'; a++)
+            for (char b = 'A'; b <= 'Z'; b++)
+                for (char c = 'A'; c <= 'Z'; c++)
+                    for (char d = 'A'; d <= 'Z'; d++)
+                        for (char e = 'A'; e <= 'Z'; e++)
+                            yield return new string(new[] { a, b, c, d, e });
     }
 
     void displaySegments(bool[] litSegments)
