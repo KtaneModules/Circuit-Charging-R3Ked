9de5af0b65ee2fa19b33e5cefc3202ae117fadbf
siky112 <unfixedacorn@gmail.com>
2025-12-14 13:07:44 -0800

    NOW it works.

--- Master Template Copiable/Assets/circuitCharging.cs
+++ Master Template Copiable/Assets/circuitCharging.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using UnityEditor.PackageManager;
-using UnityEditorInternal;
 using UnityEngine;
 using Rnd = UnityEngine.Random;
 
